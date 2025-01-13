f1b16921e512697a5071d29217e0268886df7858
Nikisha <aquaxmirielle@gmail.com>
2025-01-13 08:18:36 +0545

    Added getter methods to student class

---
+++ git/Student.java
@@ -0,0 +1,27 @@
+package git;
+
+public class Student {
+	private String name;
+	private int age;
+	
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setName(int age) {
+		this.age = age;
+	}
+}
